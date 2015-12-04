ae012a5f0581b29b34cf2ec05a6adb6e89d8b70b
mikeArt <arterburnmj@grizzlies.adams.edu>
2015-12-04 16:25:54 -0700

    Update AvR.php

--- AvR.php
+++ AvR.php
@@ -1,14 +1,3 @@
-<?php
-$servername = "sqlserver1010.database.windows.net";
-$username = "MArterburn";
-$password = "fOOTBALL33";
-
-// Create connection
-$conn = new mysqli($sqlserver1010.database.windows.net, $MArterburn, $fOOTBALL33);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-?>
+<a href="https://azuredeploy.net/" target="_blank">
+    <img src="http://azuredeploy.net/deploybutton.png"/>
+</a>
